93be0acd00fb0d7735b8cfb016944ec33710b31b
lekj67 <lekj67040@edu.ucl.dk>
2024-09-25 10:05:13 +0200

    upload af filer

--- helloworld/lektion 5/repetition.cpp
+++ helloworld/lektion 5/repetition.cpp
@@ -2,9 +2,30 @@
 
 using namespace std; 
 
-int main()  {
 
+
+int main() {
+    string kode;
+    int pss;
+
+    do {
+        cout << "Hvad er din kode?\n"; 
+        cin >> kode; 
+        pss = kode.length(); // Update the length after user input
+
+        if (pss < 8 || pss > 16) {
+            cout << "Din kode er ikke lang nok.\n";
+        } 
+        else if (pss >= 8 && pss <= 16) {
+            cout << "Din kode er lang nok.\n";
+        }
+    } while (pss < 8 || pss > 16); // Loop until valid length
+
+    // Once the loop exits, we know the length is correct
+    cout << "Din kode er god.\n";
+
+    return 0; 
+}
 
 
     
-}
