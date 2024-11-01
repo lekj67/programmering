3b572990f875fb9308c2b0fea884155f81340e69
lekj67 <lekj67040@edu.ucl.dk>
2024-11-01 11:59:14 +0100

    no clue

--- helloworld/lektion classes/customer.cpp
+++ helloworld/lektion classes/customer.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <math.h>
+#include <ctime>
+
 using namespace std; 
 class Kunde {
     private: 
@@ -13,9 +15,6 @@ class Kunde {
         void indsæt_penge (double);
         void beregn_abonnement ();
         void rente_indtægter ();
-        void rente_mat();
-
-
 };
 
 
@@ -26,47 +25,50 @@ void Kunde::indsæt_penge (double beløb) {
 
 
 void Kunde::rente_indtægter() { 
-    if ( formue < 100000) {
- rente = 0.025;
+    if (formue < 100000) {
+        rente = 0.025;
+    } else if (formue >= 100000 && formue <= 250000) {
+        rente = 0.0375;
+    } else if (formue > 250000) {
+        rente = 0.05;
+    }
+    rente_indtægt = formue * rente;
 }
 
-else if (( formue >=250000) && (formue <= 100000)) {
- rente = 0.0375;
-}
-else if ((formue > 250000) && ( formue < 500000)) {
-rente = 0.05;
-rente_indtægt = formue * rente;
 
-} 
 
-}
     
 
 void Kunde::beregn_abonnement () {
     if (formue < 100000.0) {
         abonnement = 0;
     }
-    else if (100000.0 < formue < 250000.0) {
+     else if (100000.0 > formue < 250000.0) {
         abonnement = 50;
     }
-    else if (formue >= 250000.0) {
+     else if (formue >= 250000.0) {
         abonnement = 100;
     };
 }
 
 int main () {
+time_t now = time(0);
+char* dt = ctime(&now);
+
+cout << "klokken er "  << dt << "\n"; 
+
     Kunde ucl;
+    
     std::cout << ucl.formue << std::endl;
-    ucl.formue = 1500000.0;
+    ucl.formue = 778877.0;
     ucl.beregn_abonnement();
     std::cout << ucl.formue << std::endl; 
     std::cout << ucl.abonnement << std::endl;
-    ucl.indsæt_penge(100000.0);
+    ucl.indsæt_penge(4000.0);
     ucl.beregn_abonnement();
     std::cout << ucl.formue << std::endl;
     std::cout << ucl.abonnement << std::endl;
-    ucl.rente_indtægter(); 
-ucl.rente_mat();
+         ucl.rente_indtægter(); 
     std::cout << "u have made many renter " << ucl.rente_indtægt << "\n"; 
    
 }
